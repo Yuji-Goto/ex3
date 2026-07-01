3c3d14184a9b87c42d3c2c2f14e8b195370e89ec
Yuji-Goto <s1330229@u-aizu.ac.jp>
2026-07-02 00:09:30 +0900

    HoT: initial revision

---
+++ coinToss.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  srand((unsigned int)time(NULL));
+  int h_cnt=0;
+  int t_cnt=0;
+  printf("Tossing a coin...\n");
+
+  for(int i=0;i<3;i++){
+    printf("Round %d: ",i+1);
+    if(rand()%2==0){
+      printf("Heads\n");
+      h_cnt++;
+    }
+    else{
+      printf("Tails\n");
+      t_cnt++;
+    }
+  }
+  printf("Heads: %d, Tails: %d\n",h_cnt,t_cnt);
+  return 0;
+}
